240d4e8e7c047abbc2eed6775e85b58d1e3bfbd4
Kamol Treewatchararat <ktreewatchararat@atlassian.com>
2018-04-09 21:40:55 +0700

    add gh-page and deploy to git

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import SEED_DATA from "./data.json";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
