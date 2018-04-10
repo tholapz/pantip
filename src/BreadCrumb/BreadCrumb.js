f005448c7c3e5571aa0568940ab76f716b172300
Kamol Treewatchararat <ktreewatchararat@atlassian.com>
2018-04-10 20:53:43 +0700

    bread crumb show topic title

--- src/BreadCrumb/BreadCrumb.js
+++ src/BreadCrumb/BreadCrumb.js
@@ -2,23 +2,37 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
+import { topics } from "../data.json";
 import "./BreadCrumb.css";
 
 export default class BreadCrumb extends Component {
     static props = {
-        top: PropTypes.string
+        top: PropTypes.string,
     };
 
     static defaultProps = {
-        top: ''
+        top: '',
     }
 
     render() {
+        const topicId = window.location.hash.substr(8);
+        const topic = topics.find(t => t.id === topicId);
+
         return (
             <div className="bread-crumb-container">
                 <div className="container">
-                    <Link to="/"><span className="glyphicon glyphicon-home" aria-hidden="true"/></Link>
-                    <span className="bread-crumb">{this.props.top}</span>
+                    <span className="bread-crumb">
+                        <Link to="/">
+                            <span className="glyphicon glyphicon-home" aria-hidden="true"/>
+                            { ` ${this.props.top}` }
+                        </Link>
+                    </span>
+                    { topic && (
+                        <span>
+                            <span className="glyphicon glyphicon-chevron-right" aria-hidden="true" />
+                            { ` ${topic.title}` }
+                        </span>
+                    )}
                 </div>
             </div>
         );
